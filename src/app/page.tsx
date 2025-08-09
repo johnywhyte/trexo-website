e7c83ac97afd7a966556d9c4e1b079a3a19fe3aa
Olorunfemi John <johny.whytee@gmail.com>
2025-08-09 12:10:05 +0700

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 // pages/index.tsx
 import { NextPage } from "next";
+import Logo from "@/icons/logo";
 
 const Home: NextPage = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-white  to-blue-100">
-      <div className="text-center text-black p-6 rounded-2xl ">
-        <h1 className="md:text-6xl font-bold mb-4">Trexo Technologies</h1>
+    <div className="min-h-screen  w-full flex items-center justify-center bg-gradient-to-br from-white  to-blue-100">
+      <div className="text-center w-[40rem] text-black  rounded-2xl ">
+        <div className="flex justify-center">
+          <div className="w-[10rem]">
+            <Logo />
+          </div>
+        </div>
+        <div className="flex justify-center ">
+          <h1 className="md:text-6xl font-bold mb-4">Trexo Technologies</h1>
+        </div>
         <p className="text-lg italic  mb-2">
           Building software that solves, not just sells
         </p>
